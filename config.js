a30e72b59ee9475e1fd6fc98fce3098b98ac293e
wk443311 <wk443311@gmail.com>
2026-08-18 21:01:44 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,70 @@
+// এই ফাইলে আপনি আপনার সব লিঙ্ক এবং ইমেজ পরিবর্তন করবেন
+const configData = {
+    // উপরের অ্যাড স্ক্রিপ্ট এর লিঙ্ক (যদি প্রয়োজন হয়)
+    adScriptSrc: "https://pl27924497.profitablecpmratenetwork.com/ae/bc/80/aebc8091288b74057776d2e8f16650ef.js",
+
+    // রিডাইরেক্ট সেটিংস
+    redirectSettings: {
+        firstRedirect: {
+            url: "https://omg10.com/4/9809281",
+            time: 8000 // ৮ সেকেন্ড
+        },
+        secondRedirect: {
+            url: "https://video-hub-murex-six.vercel.app",
+            time: 13000 // ১৩ সেকেন্ড
+        }
+    },
+
+    // ভিডিও পোস্টের তথ্য
+    posts: [
+        {
+            id: 1,
+            image: "https://i.ibb.co.com/DfNDddF6/1775025414376.jpg",
+            title: "UNO sir এর অস্থির লিক ভিডিও দেখতে নিচের বাটনে ক্লিক করুন",
+            buttonLink: "https://omg10.com/4/9809281",
+            links: [
+                { title: "ফুল ভিডিও দেখুন ওয়েবসাইটে 👇", url: "https://ln.run/gXNPZ" },
+                { title: "উপরের লিংক ব্যান হয়ে গেলে এখানে ক্লিক করুন", url: "https://omg10.com/4/9722283" }
+            ],
+            views: "701k",
+            reactions: { "😍": 415, "🥵": 240, "😊": 810, "❤️": 515, "❤️‍🩹": 565 }
+        },
+        {
+            id: 2,
+            image: "https://i.ibb.co.com/gZjcXkwj/cfa93da2595c630d2ea7b5ec06af40ac.webp",
+            title: "othoii এর ভিডিও দেখতে নিচের বাটনে ক্লিক করুন",
+            buttonLink: "https://otieu.com/4/9722284",
+            links: [
+                { title: "Othoi আর শশা আপার ভিডিও", url: "https://ln.run/UOIoe" },
+                { title: "প্রিমিয়াম ভিডিও চ্যানেল", url: "https://otieu.com/4/9809281" }
+            ],
+            views: "904.3k",
+            reactions: { "😍": 142, "🥵": 618, "😊": 688, "❤️": 170, "👍": 685 }
+        },
+        {
+            id: 3,
+            image: "https://i.ibb.co.com/BVGfVb8h/trending-iramoni-JPEG-7.jpg",
+            title: "ইরা মনির ভিডিও দেখতে নিচের বাটনে ক্লিক করুন",
+            buttonLink: "https://ln.run/gXNPZ",
+            links: [
+                { title: "সকল ভিডিও এই ওয়েবসাইটে", url: "https://omg10.com/4/9809281" },
+                { title: "প্রতিদিন ৫০০ থেকে ১০০০ টাকা ইনকাম করতে এখানে ক্লিক করুন", url: "https://omg10.com/4/9722283" }
+            ],
+            views: "707.4k",
+            reactions: { "😍": 205, "🥵": 258, "❤️": 140, "❤️‍🩹": 789 }
+        },
+        // এভাবে আপনি যত খুশি পোস্ট যোগ করতে পারবেন...
+        {
+            id: 4,
+            image: "https://i.ibb.co.com/6Rd2K1cg/a4d72eae030ff34d4f1bdcd96293b6aa.webp",
+            title: "মিরার ভিডিও দেখতে নিচের বাটনে ক্লিক করুন",
+            buttonLink: "https://omg10.com/4/9722283",
+            links: [
+                { title: "ভাই বোনের অস্থির বিডিও লিংক", url: "https://ln.run/UOIoe" },
+                { title: "পরিমনির ৭:৩০ সেকেন্ডের অস্থির ভিডিও লিংক", url: "https://ln.run/VNawT" }
+            ],
+            views: "857.9k",
+            reactions: { "😍": 876, "🥵": 195, "❤️": 250, "🫠": 689 }
+        }
+    ]
+};
